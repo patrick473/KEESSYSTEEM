35b138b240bd4201eb9f00805e783442ae226147
patrick kottman <patkottman@gmail.com>
2018-07-27 21:38:00 +0200

    updated most routes to use group class

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,78 +1,80 @@
-const _ = require('lodash');
+const _ = require("lodash");
+const Groupfunctions = require("../models/Group");
+const Group = Groupfunctions.GroupClass;
+const groups = Groupfunctions.Groups;
+const addUserToGroup = Groupfunctions.addUserToGroup;
 
-groups =[];
 newGroupID = 1;
 
-module.exports = (client,io) => {
-    //create gamegroup
+module.exports = (client, io) => {
+  //create gamegroup
 
-    client.on('createGroup',(data) =>{
-        
-        group = {
-            groupID : newGroupID,
-            name : data,
-            owner: [client.id],
-            users: [client.id],
-            gameRunning: false,
-            //user that needs to react
-            reactableUser: '',
-        }
-        groups.push(group);
-        newGroupID++;
-        message = "you have created group "+ group.name+ " with id :"+ group.groupID;
-        
-        io.to(client.id).emit('chatMessage', message);
-        console.log(groups);
-    }) 
-    client.on('joinGroup',(data) =>{
-        //join group, creator already joined
-        groupID = groups.findIndex(x => x.name == data);
-        groups[groupID].users.push(client.id);
-        group = groups[groupID];
-        message = "you have joined group "+ group.name+ " with id :"+ group.groupID;
-        io.to(group.owner).emit('chatMessage','A user has joined your group.');
-        io.to(client.id).emit('chatMessage', message);
-        console.log(groups);
-    }) ;
-    client.on('startGame',(data) =>{
-        groupID = groups.findIndex(x => x.name == data);
-        groups[groupID].gameRunning = true;
-        users = groups[groupID].users;
-        users.forEach(user => {
-            io.to(user).emit('chatMessage', 'game started with group:'+ group.name);
-        });
-        
-       sendReaction(groupID);
-            
-        
-        
-    });
-    client.on('reactGame',(data) =>{
-        groupID = groups.findIndex(x => x.name == data);
-        console.log(groups[groupID]);
-        if(client.id == groups[groupID].reactableUser){
-            sendReaction(groupID);
-        }
-    });
+  client.on("createGroup", data => {
+    group = new Group(newGroupID, data, client.id);
+    newGroupID++;
+    //later return group object
+    message =
+      "you have created group :" + group.name +
+      " with accesscode :" + group.accessCode.toUpperCase();
 
-    client.on('stopGame',(data) =>{
-        groupID = groups.findIndex(x => x.name == data);
-        groups[groupID].gameRunning = false;
-        io.to(client.id).emit('chatMessage', 'game stopped with group:'+ group.name);
-   
-    });
-   
-    function sendReaction(id){
-        group = groups[id];
-        randomUser = _.sample(users);
-
-        groups[id].reactableUser = randomUser;
-
-        
-        
-        io.to(randomUser).emit('reactGame','please react user:'+ groups[id].reactableUser)
-        
-    }
-  };
-  
+    io.to(client.id).emit("chatMessage", message);
+  });
+  client.on("joinGroup", data => {
+      console.log(data);
+    //join group, creator already joined
+    const group = addUserToGroup(client.id,data.toLowerCase());
     
+    //check if group is found if so return message to user and groupleader
+    //otherwise send message to user that accesscode is invalid
+    
+    if(group instanceof Group){
+
+    message =
+      "you have joined group " + group.name + " with id :" + group.groupID;
+    io.to(group.owner).emit("chatMessage", "A user has joined your group.");
+    io.to(client.id).emit("chatMessage", message);
+    
+    }
+    else{
+        io.to(client.id).emit("chatMessage",group)
+    }
+  });
+  client.on("startGame", data => {
+    groupID = groups.findIndex(x => x.name == data);
+    groups[groupID].gameRunning = true;
+    users = groups[groupID].users;
+    users.forEach(user => {
+      io.to(user).emit("chatMessage", "game started with group:" + group.name);
+    });
+
+    sendReaction(groupID);
+  });
+  client.on("reactGame", data => {
+    groupID = groups.findIndex(x => x.name == data);
+    console.log(groups[groupID]);
+    if (client.id == groups[groupID].reactableUser) {
+      sendReaction(groupID);
+    }
+  });
+
+  client.on("stopGame", data => {
+    groupID = groups.findIndex(x => x.name == data);
+    groups[groupID].gameRunning = false;
+    io.to(client.id).emit(
+      "chatMessage",
+      "game stopped with group:" + group.name
+    );
+  });
+
+  function sendReaction(id) {
+    group = groups[id];
+    randomUser = _.sample(users);
+
+    groups[id].reactableUser = randomUser;
+
+    io.to(randomUser).emit(
+      "reactGame",
+      "please react user:" + groups[id].reactableUser
+    );
+  }
+};
