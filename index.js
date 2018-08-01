ebe54988a308c65c5cd5603231e2dca0132179cb
patrick kottman <patkottman@gmail.com>
2018-08-01 21:57:21 +0200

    updated proxy rule

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
   
 
 
-    app.get('/', function(req, res){
+    app.get('/io', function(req, res){
         res.sendFile(__dirname + '/index.html');
         });
         io.on('connection', (client) =>{  
