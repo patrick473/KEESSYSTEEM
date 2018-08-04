dd1e7990298abb2d2c64e72705578f254b05c03c
patrick kottman <patkottman@gmail.com>
2018-08-04 12:35:41 +0200

    updated config for heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 
     app.get('/', function(req, res){
-        res.sendFile(__dirname + '/index.html');
+      
         });
         io.on('connection', (client) =>{  
             console.log('Client connected...');
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
         });
        
 
-const PORT = 4999
+const PORT = process.env.PORT || 5000
 http.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
